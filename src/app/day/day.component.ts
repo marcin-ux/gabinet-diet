a09a8b1e2fdd959bb3a7f11bd10627ba0da0ba57
Marcin Sz <marcin.szulzycki@aptitudesoftware.com>
2018-06-22 10:08:56 +0200

    Day

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -7,32 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DayComponent implements OnInit {
 
-  constructor() { }
+  appointments:any[];
+
+  constructor() {
+  }
 
   ngOnInit() {
-  }
-  
-  appointments:Object = [
-    {
-      "date": "Today",  
-      "time": 1330,
-      "patient": "Jan Kowalski",
-      "visit": "Kontrolna",
-      "duration": 2,
-      "cost": 100
-    },
-    {
-      "date": "Today",
-      "time": 900,
-      "patient": "Joanna Nowak",
-      "visit": "Pierwsza",
-      "duration": 3,
-      "cost": 150
-      }
-  ];
+    this.appointments = [
+      {
+        "date": "Today",  
+        "time": 1330,
+        "patient": "Jan Kowalski",
+        "visit": "Kontrolna",
+        "duration": 2,
+        "cost": 100
+      },
+      {
+        "date": "Today",
+        "time": 900,
+        "patient": "Joanna Nowak",
+        "visit": "Pierwsza",
+        "duration": 3,
+        "cost": 150
+        }
+    ];
+  } 
 
-  timetable:any = [
+
+  timetable:number[] = [
     800, 830, 900, 930, 1000, 1030, 1100, 1130, 1200, 1230, 1300, 1330, 1400, 1430, 1500, 1530, 1600, 1630, 1700, 1730, 1800, 1830, 1900, 1930
   ];
 
+  checkApp(hour) {
+    for(let app of this.appointments) {
+      if (hour == app.time) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
